b9ff1ebf2233c74d9d5e39933066d323037ae4ca
Bikal <bikaladhikari5@gmail.com>
2024-05-17 18:24:34 +1000

    reactquery custom hooks are created

--- src/Items.jsx
+++ src/Items.jsx
@@ -1,8 +1,19 @@
-import SingleItem from './SingleItem';
-const Items = ({ items }) => {
+import SingleItem from "./SingleItem";
+import { useFetchTasks } from "./reactQueryCustomHooks";
+
+const Items = () => {
+  const { isPending, isError, data } = useFetchTasks();
+
+  if (isPending) {
+    return <p style={{ marginTop: "1rem " }}>Loading...</p>;
+  }
+  if (isError) {
+    return <p style={{ marginTop: "1rem " }}>There was an error...</p>;
+  }
+
   return (
-    <div className='items'>
-      {items.map((item) => {
+    <div className="items">
+      {data.taskList.map((item) => {
         return <SingleItem key={item.id} item={item} />;
       })}
     </div>
